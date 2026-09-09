a641742f7be1362034e9f32089ab2882b20b7faa
HJ <aldldnj2@gmail.com>
2026-09-09 10:17:36 +0900

    제이쿼리2

---
+++ c40.jquery/js/study2.js
@@ -0,0 +1,14 @@
+$('.a').css('color','red');
+let txt = $('.a').html();
+// alert(txt)
+$('.a').html('<i>새로운내용</i>');
+let href = $('.b').attr('href');
+// alert(href)
+$('.b').attr('href','https://www.naver.com');
+$('.c').addClass('on');
+$('.c').removeClass('on');
+$('.b').next().addClass('on')
+$('.c').prev().html('네이버')
+$('.c').parent().css('background-color','beige')
+
+
