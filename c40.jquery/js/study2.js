a12fe4eb460634c66bf63bdaeba027c719f2a4c7
hgj <gukjuham34@gmail.com>
2026-09-15 10:08:29 +0900

    제이쿼리

--- c40.jquery/js/study2.js
+++ c40.jquery/js/study2.js
@@ -7,8 +7,18 @@ let href = $('.b').attr('href');
 $('.b').attr('href','https://www.naver.com');
 $('.c').addClass('on');
 $('.c').removeClass('on');
-$('.b').next().addClass('on')
-$('.c').prev().html('네이버')
-$('.c').parent().css('background-color','beige')
+$('.b').next().addClass('on');
+$('.c').prev().html('네이버');
+$('.c').parent().css('background-color','beige');
 
+$('#btn').click(()=>{
+    $(".frame").css('color','blue');
+});
 
+$('.list>li').click(function()
+{
+    let idx = $(this).index()
+    alert(idx);
+});
+
+//생략함수인 =>를 썼을때. this가 묶이지 않고 상위로 감. (바인딩이 되지 않음), 
