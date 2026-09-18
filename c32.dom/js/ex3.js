2d6754fe734b6f4ce4e70b6745d3fc2079ba64dc
hgj <gukjuham34@gmail.com>
2026-09-18 10:37:42 +0900

    제이쿼리-슬라이드 및 활용

--- c32.dom/js/ex3.js
+++ c32.dom/js/ex3.js
@@ -1,46 +1,43 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    const station = document.querySelector('.station');
     const train = document.querySelector('.train');
-    const btnPrev = document.querySelector('#btn-prev');
+    const btnPrev = document.querySelector("#btn-prev");
     const btnNext = document.querySelector('#btn-next');
 
     let count = 0;
-    
-    btnNext.addEventListener('click',()=>{
-        count++;
-        train.style.transition=`all 0.8s ease 0s`;
-        train.style.transform = `translateX(${-20*count}%)`;
-        if(count===4){
-            setTimeout(()=>{
-                train.style.transition=`none`;
-                train.style.transform = `translateX(0%)`;
-                count=0;
-            },800);
-            
-        }
-        
-    });
 
-    btnPrev.addEventListener('click',()=>{
-        count--;
-        if(count<0){count=0}
-        train.style.transform = `translateX(${-25*count}%)`;
-    })
+
+    // btnNext.addEventListener('click',()=>{
+    //     count++;
+    //     train.style.transition = `all 0.8s ease 0s`;
+    //     train.style.transform = `translateX(${-20*count}%)`;
+    //     if(count===4){
+    //         setTimeout(()=>{
+    //            train.style.transition = `none`;
+    //            train.style.transform = `translateX(0%)`;
+    //            count = 0;
+    //         },810);
+    //     }
+        
+    // });
+
+    // btnPrev.addEventListener('click',()=>{
+    //     count--;
+    //     if(count<0){count=3}
+    //     train.style.transform = `translateX(${-20*count}%)`;
+    // });
 
     let timer = setInterval(()=>{
         count++;
-        train.style.transition=`all 0.8s ease 0s`;
+        train.style.transition = `all 0.8s ease 0s`;
         train.style.transform = `translateX(${-20*count}%)`;
         if(count===4){
             setTimeout(()=>{
-                train.style.transition=`none`;
-                train.style.transform = `translateX(0%)`;
-                count=0;
-            },800);
-            
+               train.style.transition = `none`;
+               train.style.transform = `translateX(0%)`;
+               count = 0;
+            },810);
         }
     },3000);
-
 
     train.addEventListener('mouseenter',()=>{
         clearInterval(timer);
@@ -49,14 +46,16 @@ document.addEventListener('DOMContentLoaded',()=>{
     train.addEventListener('mouseleave',()=>{
         timer = setInterval(()=>{
             count++;
-            train.style.transition=`all 0.8s ease 0s`;
+            train.style.transition = `all 0.8s ease 0s`;
             train.style.transform = `translateX(${-20*count}%)`;
             if(count===4){
                 setTimeout(()=>{
-                    train.style.transition=`none`;
-                    train.style.transform = `translateX(0%)`;
-                    count=0;
-                },810);            
-        }
-    },3000);        
+                train.style.transition = `none`;
+                train.style.transform = `translateX(0%)`;
+                count = 0;
+                },810);
+            }
+        },3000);
+    });
+
 });
